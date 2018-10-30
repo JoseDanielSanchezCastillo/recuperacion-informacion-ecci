0414e3c0857a7431fe8cce55afdefc0db383b0da
JoseDanielSanchezCastillo <jose.sanchezcastillo@ucr.ac.cr>
2018-10-30 11:52:44 -0600

    Agregado Frameworks de estilos

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		//$this->load->view('structure/header');
 		$this->load->view('welcome_message');
+		//$this->load->view('structure/footer');
 	}
 }
---
+++ application/views/structure/footer.php
@@ -0,0 +1,9 @@
+    <footer>
+        <div class="col-md-12" style="text-align:center;">
+            <hr>
+            Universidad de Costa Rica</a>
+        </div>
+    </footer>
+    </div>
+    </body>
+</html>
