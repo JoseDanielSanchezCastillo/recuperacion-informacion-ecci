c02cfb3cf3554c9ef0755be6bcf10641cc6aca02
KevinWV <kevin.waltam@gmail.com>
2018-11-23 04:36:31 -0600

    Falta codigo ejemplo intro

--- application/views/que_es_k_means.php
+++ application/views/que_es_k_means.php
@@ -1,11 +1,32 @@
     <div id="texto-principal" >
     	<h2 class="display-3" >¿Qué es K-means?</h2>
     	<br>
-    	<p class="text-muted mb-0">
-    		Praesent sollicitudin, lectus vel finibus egestas, neque tellus ornare nunc, lobortis efficitur quam massa imperdiet lectus. Proin condimentum ex orci, at ultrices lacus posuere quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in lorem non est aliquam dapibus. Duis suscipit est ac arcu accumsan, at dignissim massa convallis. Sed ut lectus id mauris molestie venenatis pretium sit amet ipsum. Duis condimentum ligula id erat hendrerit, sed suscipit mauris pharetra. Ut eu tellus congue, pellentesque dolor ac, pharetra ligula. Nam scelerisque, erat a consequat consequat, justo lacus molestie dui, vel auctor odio enim sed dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras et dui ut nisl feugiat tristique. Maecenas vulputate odio ut est eleifend auctor. Fusce in nisl nunc. Suspendisse potenti. Vestibulum molestie ac turpis nec ullamcorper. Cras lobortis sem a egestas viverra.
+    	<p >
+            K-means es un algoritmo de clustering plano, politético y disjunto, que usa el refinamiento iterativo para producir un k número de grupos de datos.
     	</p>
     	<br>
-    	<p class="text-muted mb-0">
-    		Maecenas sodales convallis tortor id consectetur. Proin metus risus, condimentum tempor mi vitae, feugiat congue diam. Integer nec lorem id nulla hendrerit scelerisque. In ac orci posuere, blandit velit et, maximus mi. Ut mattis mollis justo at tincidunt. Morbi lacinia vehicula felis, ac dapibus lacus aliquet nec. Vestibulum blandit leo consequat sapien sagittis, sit amet volutpat ex blandit. Phasellus efficitur aliquet justo id tristique. Aenean fermentum gravida orci vel tincidunt. Morbi mollis tellus ut ligula porta dignissim. Sed consectetur finibus sem, vel pulvinar urna suscipit vel. Donec lorem urna, ultrices quis interdum ut, eleifend ut velit. Donec a vestibulum purus. Quisque cursus tortor vel augue tristique, ac aliquet risus hendrerit. Suspendisse porttitor, lorem ac porta tristique, quam magna fringilla neque, dignissim auctor leo est eget massa. Curabitur molestie viverra mattis.
+    	<p>
+            Parámetros del algoritmo: 
     	</p>
+        <ul> 
+            <li><p>Número de grupos  por crear.</p></li> 
+            <li><p>El conjunto de datos.</p></li> 
+            <li><p>Centroides iniciales (o se pueden seleccionar aleatoriamente entre el conjunto de datos).</p></li> 
+        </ul>
+        <br>
+         <h2 class="display-3" >Algoritmo:</h2>
+        <br>
+        <p >
+            <strong>Paso 1</strong> asignación de datos 
+        </p>
+        <p >
+            Cada centroide define uno de los grupos. En este paso, cada punto de datos se asigna a su centroide más cercano, en función de la distancia euclidiana al cuadrado.
+        </p>
+        <br>
+        <p >
+            <strong>Paso 2</strong>: actualización de los centroides
+        </p>
+        <p >
+            Se recalculan los centroides (esto se hace tomando la distancia media de cada punto para con todos los demás).
+        </p>
     </div>
